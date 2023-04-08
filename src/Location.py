715194017d3b1e653ee08bfb32854263db0c34b9
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-04-08 17:24:13 +0700

    feat: added addNeighbour function and comments

--- src/Location.py
+++ src/Location.py
@@ -1,12 +1,23 @@
 class Location:
+    '''
+    Class Location for each location in map
+    '''
+
     # CONSTRUCTOR
     def __init__(self, name, latitude, longitude):
+        '''
+        name: name of the location
+        latitude: latitude of the location
+        longtitude: longtitude of the location
+        neighbour: list of string, neighbour of the location
+        '''
         self.name = name
         self.latitude = latitude
         self.longitude = longitude
         self.weight = dict() # Dictionary of location, untuk menunjukkan weight antar lokasi yang bertetangga dengan lokasi ini
+        self.neighbour = []
 
-    # STRING RETURN FOR DEBUGGING
+    # STRING REPRESENTATION OF Location
     def __str__(self):
         return f"{self.name}, {self.latitude}, {self.longitude}"
     
@@ -26,4 +37,8 @@ class Location:
     # Key dictionarynya lokasi tetangganya
     def addWeight(self, Loc, weight):
         self.weight[Loc] = weight
+
+    # ADD NEIGHBOUR
+    def addNeighbour(self, neighbour):
+        self.neighbour.append(neighbour)
         
